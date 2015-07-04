62d9f2cdb49ac51e237b2013278b722596a4b69d
Ailton Luiz Dias Siqueira Junior <ailton@iftm.edu.br>
2015-07-04 11:20:11 -0300

    Adicionado a mensagem do sábado.

--- ExemploGitEclipse/src/br/edu/iftm/HelloGit/HelloGit.java
+++ ExemploGitEclipse/src/br/edu/iftm/HelloGit/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo!!!");
+		System.out.println("Olá Mundo no sábado!!!");
 	}
 
 }
