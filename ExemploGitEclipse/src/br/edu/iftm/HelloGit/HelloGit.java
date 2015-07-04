6536cf31b6cf8439c0ee63e055afdaa51194daaf
Ailton Luiz Dias Siqueira Junior <ailton@iftm.edu.br>
2015-07-04 11:16:30 -0300

    Commit inicial.

---
+++ ExemploGitEclipse/src/br/edu/iftm/HelloGit/HelloGit.java
@@ -0,0 +1,10 @@
+package br.edu.iftm.HelloGit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!!!");
+	}
+
+}
